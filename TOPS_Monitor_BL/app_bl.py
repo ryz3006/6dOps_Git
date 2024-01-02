9a78e71e88fe55de2fab006cdfcc02f23fbf885b
Riyas Siddikk <riyass.ayath@gmail.com>
2024-01-02 16:09:26 +0300

    port updated

--- TOPS_Monitor_BL/app_bl.py
+++ TOPS_Monitor_BL/app_bl.py
@@ -143,4 +143,4 @@ def kpi_stats_submit():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5002)
+    app.run(debug=True, host="0.0.0.0", port=5070)
